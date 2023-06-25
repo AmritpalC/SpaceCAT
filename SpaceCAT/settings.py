4896ce83128f02587d72c3aa7aaee11677fba310
Puzzles65 <chitrungnguyen12@gmail.com>
2023-06-25 20:37:54 +0100

    viewing picture upon clicking

--- SpaceCAT/settings.py
+++ SpaceCAT/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ 
 
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'main_app/static'),]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
